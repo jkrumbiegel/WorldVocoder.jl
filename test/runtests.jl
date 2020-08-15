f55f670b0274e5ed98145f2d6fe0de312ac0cd6f
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-08-15 14:46:15 +0200

    add basic test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,10 @@ using WorldVocoder
 using Test
 
 @testset "WorldVocoder.jl" begin
-    # Write your tests here.
+    
+    fs = 44100
+    x = [sinpi(2t * 200) for t in 0:1/fs:5]
+    f0, timestamps = dio(x, fs)
+    spectrogram = cheaptrick(x, fs, timestamps, f0)
+    @show extrema(spectrogram)
 end
