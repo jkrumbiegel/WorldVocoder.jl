c8ac3d32cabdad3db13bc16b9425d9710ea6b991
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-08-17 10:44:48 +0200

    add synthesis type params and docstring

--- src/synthesis.jl
+++ src/synthesis.jl
@@ -1,4 +1,10 @@
-function synthesis(f0, spectrogram, aperiodicity, fs, frame_period = DEFAULT_FRAME_PERIOD)
+"""
+    synthesis(f0::Vector{Float64}, spectrogram::Matrix{Float64}, aperiodicity::Matrix{Float64}, fs::Int, frame_period::Float64 = DEFAULT_FRAME_PERIOD)
+
+Synthesize an audio signal `y::Vector{Float64}` using WORLD from and `f0` track, a spectrogram, and an aperiodicity map, at the sampling rate `fs` per second.
+Optionally specify a `frame_period` in milliseconds.
+"""
+function synthesis(f0::Vector{Float64}, spectrogram::Matrix{Float64}, aperiodicity::Matrix{Float64}, fs::Int, frame_period::Float64 = DEFAULT_FRAME_PERIOD)
 
     if size(spectrogram) != size(aperiodicity)
         error("Sizes of spectrogram $(size(spectrogram)) and aperiodicity $(size(aperiodicity)) don't match.")
